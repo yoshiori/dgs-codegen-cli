e845a14bf782b67d174e6d4ffda894f42c1c4787
hogelog <konbu.komuro@gmail.com>
2021-02-14 00:16:41 +0900

    Build graphql-dgs-codegen-core 4.1.1

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.mikeneck.graalvm.GenerateNativeImageConfigTask
+
+plugins {
+  id("java")
+  id("org.mikeneck.graalvm-native-image") version "v1.0.0"
+}
+
+repositories {
+  mavenCentral()
+  jcenter()
+}
+
+dependencies {
+  implementation("com.netflix.graphql.dgs.codegen:graphql-dgs-codegen-core:4.1.1")
+}
+
+nativeImage {
+    graalVmHome = System.getenv("JAVA_HOME")
+    mainClass ="com.netflix.graphql.dgs.codegen.CodeGenCliKt"
+    executableName = "dgs-codegen"
+    outputDirectory = file("$buildDir/executable")
+    arguments(
+        "--no-fallback",
+        "--enable-all-security-services",
+        "--report-unsupported-elements-at-runtime"
+    )
+}
